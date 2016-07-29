97bba63b488de73f531ec02478597e2e76289133
hvpulok <hvpulok@gmail.com>
2016-07-29 17:15:43 -0600

    Add carousel

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,27 @@
  * Controller of the healthyLivingApp
  */
 angular.module('healthyLivingApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function($scope) {
+        var baseURL = 'http://lorempixel.com/960/450/';
+        $scope.setInterval = 5000;
+
+        // //definition of slides model
+        $scope.slides = [{
+            id: 0,
+            title: '7 ways to stay Fit',
+            image: baseURL + 'sports/',
+            text: 'Play a sport for 30 minutes a day!'
+        }, {
+            id: 1,
+            title: 'Healthy Fit',
+            image: baseURL + 'food/',
+            text: 'Food that you should be eating!'
+        }, {
+            id: 2,
+            title: 'Relaxing Holidays',
+            image: baseURL + 'nature/',
+            text: '10 locations for nature lovers!'
+        }];
+
+        // end of controller
+    });
