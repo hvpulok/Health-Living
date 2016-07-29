3b63e39ee1b2c6f877ccecf1db2c240e6180db36
hvpulok <hvpulok@gmail.com>
2016-07-29 15:06:16 -0600

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name healthyLivingApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the healthyLivingApp
+ */
+angular.module('healthyLivingApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name healthyLivingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the healthyLivingApp
+ */
+angular.module('healthyLivingApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
