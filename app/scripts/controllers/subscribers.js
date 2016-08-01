9dcf8ebf40ef45465ce818bf94810ac83b757e7e
hvpulok <hvpulok@gmail.com>
2016-08-01 13:06:44 -0600

    Add New Subscriber Modal

--- app/scripts/controllers/subscribers.js
+++ app/scripts/controllers/subscribers.js
@@ -8,7 +8,7 @@
  * Controller of the healthyLivingApp
  */
 angular.module('healthyLivingApp')
-  .controller('SubscribersCtrl', function($scope, $http) {
+  .controller('SubscribersCtrl', function($scope, $http, $uibModal) {
     $http.get('http://localhost:9000/subscribers.json').success(function(data) {
       $scope.subscribers = data;
     });
@@ -24,5 +24,41 @@ angular.module('healthyLivingApp')
         ]
     };
 
+    $scope.showModal = function() {
+      $scope.newUser = {};
+      var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'views/add_user.html',
+        controller: 'AddNewUserCtrl',
+        // size: 'lg',
+        resolve: {
+          newUser: function(){
+            return $scope.newUser;
+          }
+        }
+      });
 
-  });
+      modalInstance.result.then(function(newUser){
+        $scope.newUser = newUser;
+        $scope.subscribers.push({
+          no: $scope.subscribers.length+1,
+          name: $scope.newUser.name,
+          userType: $scope.newUser.userType,
+          loyalty: $scope.newUser.loyalty,
+          joinDate: $scope.newUser.joinDate.toDateString()
+        });
+      });
+    };
+
+  }) // end of SubscribersCtrl control function
+
+.controller('AddNewUserCtrl', function($scope, $uibModalInstance, newUser) {
+  $scope.newUser = newUser;
+  $scope.saveNewUser = function() {
+    $uibModalInstance.close(newUser);
+  };
+
+  $scope.cancel = function() {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
